d10c160ae97216788032ac8eacd235eb181b9a89
kishan_NC <kishanth.navicode@gmail.com>
2025-07-30 10:50:16 +0530

    ok

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -2,5 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      include: ["**/*.jsx", "**/*.js"], // Handles .js and .jsx
+    }),
+  ],
+  esbuild: {
+    loader: "jsx",
+    include: [/src\/.*\.js$/, /src\/.*\.jsx$/], // ✅ Add .jsx here
+  },
 });
